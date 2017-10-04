e333b6418fe97ea55e86d08cbf51b794ec15e050
ahsannazim <ahsan.azim@hotmail.com>
2017-10-04 14:06:07 +0200

    ahsan

--- booking_ansvarlig.php
+++ booking_ansvarlig.php
@@ -7,7 +7,7 @@
   <head>
     <meta charset="utf-8">
 
-    <title> Manager </title>
+    <title> Booking Ansvarlig </title>
     <link rel="stylesheet" type="text/css" href="CSS\standard.css">
     <style type="text/css"></style>
 
@@ -35,14 +35,34 @@
 
     <main id="Main-content">
       <div class="registering_band_via_manager">
-          <h3></h3>
-          <p>
-          </p>
           <form class="band-form">
             <table>
+              <tr>
+                    <td><label for="navn"> <h4>  Tekniskebehov fra Manager: </h4></label></td>
+                      <td> <!--the data from the database is gonna be displayed here from the technical spefications-->    </td>
+                </tr>
                 <tr>
-                    <td><label for="navn">  Technical spesifications from the manager: </label></td>
-                      <td>  <!--the data from the database is gonna be displayed here from the technical spefications-->    </td>
+                    <td><label for="navn"> <h4> Tilbud for Manager </h4></label></td>
+                </tr>
+                <tr>
+                    <td><label for="pris"> Pris: </label></td>
+                  <td><input type="text" name="pris"/> kr,- </td>
+                </tr>
+                <tr>
+                    <td><label for="dato"> Dato: </label></td>
+                  <td><input type="text" name="dato"/></td>
+                </tr>
+                <tr>
+                    <td><label for="starttid"> Start Tid: </label></td>
+                  <td><input type="text" name="starttid"/></td>
+                </tr>
+                <tr>
+                    <td><label for="slutttid"> Slutt Tid: </label></td>
+                  <td><input type="text" name="slutttid"/></td>
+                </tr>
+                <tr>
+                    <td></td>
+                  <td><button> Send Tilbud </button></td> <!--tilbud blir sendt til Bookingsjef-->
                 </tr>
               </table>
           </form>
