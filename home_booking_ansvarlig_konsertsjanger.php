1d0a8b351c949bef3a2c88f9aa7251c5e06902f6
Hakon <hakon0809hotmail.com>
2017-10-26 12:01:47 +0200

    fikset min side link

--- home_booking_ansvarlig_konsertsjanger.php
+++ home_booking_ansvarlig_konsertsjanger.php
@@ -43,9 +43,9 @@
                 <!-- Henter nav linker, forms, og andre innhold for aktivering til navbaren-->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   <ul class="nav navbar-nav">
-                    <li><a> Min Side </a></li>
+                    <li><a href= <?php echo "home_" . $_SESSION["role"] . ".php"; ?>> Min Side </a></li>
                     <li ><a href="konsertoversikt.php"> Konsert Oversikt</a></li>
-                    <li class="active"><a>Sjanger<span class="sr-only">(current)</span></a></li>
+                    <li class="active"><a>Tidligere konserter<span class="sr-only">(current)</span></a></li>
                   </ul>
                   <ul class="nav navbar-nav navbar-right">
                     <li><a href="Log_In/login.php"> Logg Ut</a></li>
