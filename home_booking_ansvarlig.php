f61d80fdfe0ba780cc8076f8f5705d62d616d5a2
fredriksvendsen <fredriksvendsen2@gmail.com>
2017-11-07 03:33:50 +0100

    liste for ikke godkjent (home bans), sendtilbud add

--- home_booking_ansvarlig.php
+++ home_booking_ansvarlig.php
@@ -1,11 +1,11 @@
 <?php
-  session_start();
-  include("config.php");
+session_start();
+include("config.php");
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <title> Bookingansvarlig </title>
 
@@ -18,115 +18,115 @@
     <!-- Lokal CSS/JS -->
     <link rel="stylesheet" type="text/css" href="CSS/standard.css">
     <style type="text/css"></style>
-  </head>
+</head>
 
-  <body>
-    <h1 id="overskrift1">Dagene</h1>
-    <div class="container top-container">
-        <!-- Header -->
-        <div class="page-header">
-            <!-- Meny-stripe hentet fra bootstrap tutorials -->
-            <nav class="navbar navbar-default">
-              <div class="container-fluid">
+<body>
+<h1 id="overskrift1">Dagene</h1>
+<div class="container top-container">
+    <!-- Header -->
+    <div class="page-header">
+        <!-- Meny-stripe hentet fra bootstrap tutorials -->
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
                 <!-- Brand og veksle blir gruppert for bedre utsikt for mobil utstilling -->
                 <div class="navbar-header">
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <span class="navbar-brand">Forside</span>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <span class="navbar-brand">Forside</span>
                 </div>
 
                 <!-- Henter nav linker, forms, og andre innhold for aktivering til navbaren-->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  <ul class="nav navbar-nav">
-                    <li class="active"><a href= <?php echo "home_" . $_SESSION["role"] . ".php"; ?>> Min Side <span class="sr-only">(current)</span> </a></li>
-                    <li><a href="konsertoversikt.php">Konsertoversikt</a></li>
-                    <li><a href="home_booking_ansvarlig_konsertsjanger.php">Tidligere konserter</a></li>
-                  </ul>
-                  <ul class="nav navbar-nav navbar-right">
-                    <li><a href="Log_In/login.php"> Logg ut</a></li>
-                  </ul>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href= <?php echo "home_" . $_SESSION["role"] . ".php"; ?>> Min Side <span class="sr-only">(current)</span> </a></li>
+                        <li><a href="konsertoversikt.php">Konsertoversikt</a></li>
+                        <li><a href="home_booking_ansvarlig_konsertsjanger.php">Tidligere konserter</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="Log_In/login.php"> Logg ut</a></li>
+                    </ul>
                 </div><!-- /.navbar-collapse -->
-              </div><!-- /.container-fluid -->
-            </nav>
-          </div>
-        </div>
+            </div><!-- /.container-fluid -->
+        </nav>
+    </div>
+</div>
 
-        <main id="Main-content">
+<main id="Main-content">
 
-              <script type="text/javascript" language="javascript">
-              function selectChange(val1) {
-              //Set the value of action in action attribute of form element.
-              //Submit the form
-              $('#myForm').submit();}
-              </script>
+    <script type="text/javascript" language="javascript">
+        function selectChange(val1) {
+            //Set the value of action in action attribute of form element.
+            //Submit the form
+            $('#myForm').submit();}
+    </script>
 
-              <div id="dropdownmenu">
-                  <?php
-                    $sql = "SELECT band.b_id, users.name
+    <div id="dropdownmenu">
+        <?php
+        $sql = "SELECT band.b_id, users.name
                             FROM users INNER JOIN band
                             ON users.u_id = band.manager_id
                             AND band.bans_id = '$_SESSION[user_id]'" ;
+        $result = $conn->query($sql);
+        echo "<form id='myForm' method = 'post'>";
+        echo "<select name='band_id' onChange=selectChange(this.value)>";
+        echo "<option hidden> Velg manager </option>";
+        while ($row = $result->fetch_assoc()){
+            echo "<option value=" . $row['b_id'] . ">" . $row['name'] . "</option>";
+        }
+        echo "</select>";
+        echo "</form>";
+        ?>
+    </div>
+
+    <table>
+        <tr>
+            <td><label>  Tekniske behov fra band: </label></td>
+            <td>
+                <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                    $sql = "SELECT behov FROM teknisk_behov WHERE band_id = '$_POST[band_id]'";
                     $result = $conn->query($sql);
-                    echo "<form id='myForm' method = 'post'>";
-                    echo "<select name='band_id' onChange=selectChange(this.value)>";
-                    echo "<option hidden> Velg manager </option>";
-                    while ($row = $result->fetch_assoc()){
-                      echo "<option value=" . $row['b_id'] . ">" . $row['name'] . "</option>";
+
+                    //makes a table with the info
+                    if ($result->num_rows > 0) {
+                        echo "<table>";
+                        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                            echo "<tr><td>" . $row["behov"] . "</td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "Ingen behov lagt inn";
                     }
-                    echo "</select>";
-                    echo "</form>";
-                    ?>
-                  </div>
+                }
+                $conn->close();
+                ?>
+            </td>
+        </tr>
+    </table>
 
-                      <table>
-                        <tr>
-                          <td><label>  Tekniske behov fra band: </label></td>
-                            <td>
-                            <?php
-                              if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    <label><h4> Nytt tilbud til manager </h4></label>  <!-- for å finne tilbud til manager og godkjenne den -->
 
-                                $sql = "SELECT behov FROM teknisk_behov WHERE band_id = '$_POST[band_id]'";
-                                $result = $conn->query($sql);
-
-                                //makes a table with the info
-                                if ($result->num_rows > 0) {
-                                  echo "<table>";
-                                  while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                                    echo "<tr><td>" . $row["behov"] . "</td></tr>";
-                                  }
-                                    echo "</table>";
-                                  } else {
-                                    echo "Ingen behov lagt inn";
-                                  }
-                                }
-                                $conn->close();
-                              ?>
-                            </td>
-                          </tr>
-                        </table>
-
-          <label><h4> Nytt tilbud til manager </h4></label>  <!-- for å finne tilbud til manager og godkjenne den -->
-
-         <div class="registering_band_via_manager">
-                <form action="sendtilbudBA.php" method="post">
-                    <table>
-                    <tr>
-                    <tr>
-                        <td><label> Artist: </label></td>
-                        <td><input id="artist" name="artist" type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td><label> Pris: </label></td>
-                      <td><input id="pris" name="pris" type="number"/> kr,- </td>
-                    </tr>
-                    <tr>
-                        <td><label> Scene For Konserten: </label></td>
-                      <td>
-                          <select name="scene" id="scene">
+    <div class="registering_band_via_manager">
+        <form action="sendtilbudBA.php" method="post">
+            <table>
+                <tr>
+                <tr>
+                    <td><label> Artist: </label></td>
+                    <td><input id="artist" name="artist" type="text"/></td>
+                </tr>
+                <tr>
+                    <td><label> Pris: </label></td>
+                    <td><input id="pris" name="pris" type="number"/> kr,- </td>
+                </tr>
+                <tr>
+                    <td><label> Scene For Konserten: </label></td>
+                    <td>
+                        <select name="scene" id="scene">
                             <option hidden>Velg scene</option>
                             <?php
                             include ("config.php");
@@ -138,72 +138,72 @@
                                 }
                             }
                             ?>
-                          </select>
-                      </td>
-                    </tr>
-                    <tr>
-                        <td><label> Dato For Konserten: </label></td>
-                      <td><input id="datokonsert" name="datokonsert" type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td><label> Dato Tilbud Blir Sendt: </label></td>
-                      <td><input  id="datosend" name="datosend" type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td><label> Konsert Start Tid: </label></td>
-                      <td><input id="konsertstart" name="konsertstart" type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td><label> Konsert Slutt Tid: </label></td>
-                      <td><input id="konsertslutt" name="konsertslutt" type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td><label> Omtale </label></td>
-                        <td>
-                          <textarea id="omtale" name="omtale" type="text" rows="5" cols="40"></textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> Lenker til Presseomtaler </label></td>
-                        <td>
-                          <textarea id="presseomtale" name="presseomtale" type="text" rows="5" cols="40"></textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><label> Melding til Manager </label></td>
-                      <td>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label> Dato For Konserten: </label></td>
+                    <td><input id="datokonsert" name="datokonsert" type="text"/></td>
+                </tr>
+                <tr>
+                    <td><label> Dato Tilbud Blir Sendt: </label></td>
+                    <td><input  id="datosend" name="datosend" type="text"/></td>
+                </tr>
+                <tr>
+                    <td><label> Konsert Start Tid: </label></td>
+                    <td><input id="konsertstart" name="konsertstart" type="text"/></td>
+                </tr>
+                <tr>
+                    <td><label> Konsert Slutt Tid: </label></td>
+                    <td><input id="konsertslutt" name="konsertslutt" type="text"/></td>
+                </tr>
+                <tr>
+                    <td><label> Omtale </label></td>
+                    <td>
+                        <textarea id="omtale" name="omtale" type="text" rows="5" cols="40"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label> Lenker til Presseomtaler </label></td>
+                    <td>
+                        <textarea id="presseomtale" name="presseomtale" type="text" rows="5" cols="40"></textarea>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label> Melding til Manager </label></td>
+                    <td>
                         <textarea id="meldingm" name="meldingm" type="text" rows="5" cols="40" ></textarea>
-                      </td>
-                    </tr>
-                    <tr>
-                        <td><label> Manager e-mail </label></td>
-                        <td><input id="mail_m" type="email" name="mail_m"/></td>
-                    </tr>
-                    <tr>
-                        <td><label> Melding til Bookingsjef: </label></td>
-                      <td>
+                    </td>
+                </tr>
+                <tr>
+                    <td><label> Manager e-mail </label></td>
+                    <td><input id="mail_m" type="email" name="mail_m"/></td>
+                </tr>
+                <tr>
+                    <td><label> Melding til Bookingsjef: </label></td>
+                    <td>
                         <textarea id="meldingbs" name="meldingbs" type="text"rows="5" cols="40" ></textarea>
-                      </td>
-                    </tr>
-                        <td></td>
-                      <td> <input id="btn" type="submit" name="submit" value="Send"/>
-                      </td> <!--tilbud blir sendt til Bookingsjef-->
-                    </tr>
-                  </table>
-              </form>
-          </div>
-            <h1>Avslåtte tilbud</h1>
-            <?php
-            include("config.php");
-            $sql = "SELECT t_id, t_artist_name, t_pris, t_scene, t_dato_k, t_dato_sendt, t_tidkonsertstart, t_tidkonsertslutt, melding_til_m, mail_m, melding_til_bs, godkjent_bs, godkjent_m, tilbud_sendt_til_bs, tilbud_sendt_til_m, fra_bans_id
+                    </td>
+                </tr>
+                <td></td>
+                <td> <input id="btn" type="submit" name="submit" value="Send"/>
+                </td> <!--tilbud blir sendt til Bookingsjef-->
+                </tr>
+            </table>
+        </form>
+    </div>
+    <h1>Avslåtte tilbud</h1>
+    <?php
+    include("config.php");
+    $sql = "SELECT t_id, t_artist_name, t_pris, t_scene, t_dato_k, t_dato_sendt, t_tidkonsertstart, t_tidkonsertslutt, melding_til_m, mail_m, melding_til_bs, godkjent_bs, godkjent_m, tilbud_sendt_til_bs, tilbud_sendt_til_m, fra_bans_id
                               FROM tilbud WHERE godkjent_bs = 2
                               AND fra_bans_id = $_SESSION[user_id]
                               ";
-            $result = $conn->query($sql);
+    $result = $conn->query($sql);
 
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "
                         <button type='button' id='0' onclick='moreOrLess(this.id)' class='artistButton'>Artist: " . $row["t_artist_name"] . "</button>
                         <table class='tilbudText' style='display:none'>
                         <input class='o' name='tilbudid' id='tilbudid' style='display:none' value='" . $row["t_id"] . "'/>
@@ -217,46 +217,48 @@
                         <tr><td><label> Melding til Manager: </label></td><td> " . $row["melding_til_m"] . "</td></tr>
                         <tr><td><label> Manager e-mail: </label></td><td> " . $row["mail_m"] . "</td></tr>
                         <tr><td><label> Melding fra Bookingansvarlig: </label></td><td> " . $row["melding_til_bs"] . "</td></tr>
-                        <tr><td><label> Godkjenn Tilbud: </label><input id=\"btn\" value=\"Godkjenn\"/></td><td><input class='avslaaBtn' id=\"avslaaBtn\" value=\"Avslå tilbud\"/></td></tr><!-- onclick='setID(this.id)'-->
-
                         </table><br>
                         </form>";
-                }
+        }
+    } else {
+        echo "Ingen tilbud avslått";
+    }
+    $conn->close();
+    ?>
+    <?php
+    include("config.php");
+    echo "<script>console.log('test$_SESSION[user_id]')</script>";
+    ?>
+    <script>
+        var artistList = document.getElementsByClassName("artistButton");
+        var tekstList = document.getElementsByClassName("tilbudText");
+        /*
+        var avslaaList = document.getElementsByClassName("o");
+        var avslaaBtnList = document.getElementsByClassName("avslaaBtn");
+        */
+
+        for(var i=0;i<=artistList.length;i++) {
+            artistList[i].id = i;
+            //tekstList[i].id = "0" + i;
+            //avslaaBtnList[i].id = i;
+
+        }
+
+        function moreOrLess(i) {
+            if (tekstList[i].style.display == "none") {
+                tekstList[i].style.display = "block";
             } else {
-                echo "Ingen konserter";
+                tekstList[i].style.display = "none";
             }
-            $conn->close();
-            ?>
-            <script>
-                var artistList = document.getElementsByClassName("artistButton");
-                var tekstList = document.getElementsByClassName("tilbudText");
-                /*
-                var avslaaList = document.getElementsByClassName("o");
-                var avslaaBtnList = document.getElementsByClassName("avslaaBtn");
-                */
+        }
 
-                for(var i=0;i<=artistList.length;i++) {
-                    artistList[i].id = i;
-                    //tekstList[i].id = "0" + i;
-                    //avslaaBtnList[i].id = i;
-
-                }
-
-                function moreOrLess(i) {
-                    if (tekstList[i].style.display == "none") {
-                        tekstList[i].style.display = "block";
-                    } else {
-                        tekstList[i].style.display = "none";
-                    }
-                }
-
-                /*
-                function setID(i) {
-                    avslaaList[i].id = "tilbudid";
-                }
-                */
-            </script>
-        </main>
-        </body>
+        /*
+        function setID(i) {
+            avslaaList[i].id = "tilbudid";
+        }
+        */
+    </script>
+</main>
+</body>
 
 </html>
--- sendtilbudBA.php
+++ sendtilbudBA.php
@@ -1,25 +1,26 @@
 <?php
-  include('config.php');
+include('config.php');
 
-  $artist = $_POST['artist'];
-  $pris = $_POST['pris'];
-  $scene = $_POST['scene'];
-  $datokonsert = $_POST['datokonsert'];
-  $datosend = $_POST['datosend'];
-  $konsertstart = $_POST['konsertstart'];
-  $konsertslutt = $_POST['konsertslutt'];
-  $meldingm = $_POST['meldingm'];
-  $mail_m = $_POST['mail_m'];
-  $meldingbs = $_POST['meldingbs'];
-  $omtale = $_POST['omtale'];
-  $presseomtale = $_POST['presseomtale'];
+$artist = $_POST['artist'];
+$pris = $_POST['pris'];
+$scene = $_POST['scene'];
+$datokonsert = $_POST['datokonsert'];
+$datosend = $_POST['datosend'];
+$konsertstart = $_POST['konsertstart'];
+$konsertslutt = $_POST['konsertslutt'];
+$meldingm = $_POST['meldingm'];
+$mail_m = $_POST['mail_m'];
+$meldingbs = $_POST['meldingbs'];
+$omtale = $_POST['omtale'];
+$presseomtale = $_POST['presseomtale'];
+$userid = $_SESSION['user_id'];
 
-  $sql = "INSERT INTO tilbud (t_artist_name, t_pris, t_scene, t_dato_k, t_dato_sendt, t_tidkonsertstart, t_tidkonsertslutt, melding_til_m, mail_m, melding_til_bs, tilbud_sendt_til_bs, tilbud_sendt_til_m, omtaler, presseomtaler)
-          VALUES ('$artist', '$pris', '$scene', '$datokonsert', '$datosend', '$konsertstart', '$konsertslutt', '$meldingm', '$mail_m', '$meldingbs', 'Ja', 'Nei', '$omtale', '$presseomtale' )";
-  if ($conn->query($sql) === TRUE) {
+$sql = "INSERT INTO tilbud (t_artist_name, t_pris, t_scene, t_dato_k, t_dato_sendt, t_tidkonsertstart, t_tidkonsertslutt, melding_til_m, mail_m, melding_til_bs, tilbud_sendt_til_bs, tilbud_sendt_til_m, omtaler, presseomtaler, fra_bans_id)
+          VALUES ('$artist', '$pris', '$scene', '$datokonsert', '$datosend', '$konsertstart', '$konsertslutt', '$meldingm', '$mail_m', '$meldingbs', 'Ja', 'Nei', '$omtale', '$presseomtale', '$userid' )";
+if ($conn->query($sql) === TRUE) {
     echo "New record created successfully!";
-  } else {
+} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  header("refresh:3; url= home_booking_ansvarlig.php");
+}
+header("refresh:3; url= home_booking_ansvarlig.php");
 ?>
